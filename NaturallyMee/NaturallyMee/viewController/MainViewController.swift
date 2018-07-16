0799bf2d8d80e283cad63de151b139d6a4d35624
SarinSwift <sarinyaswift@gmail.com>
2018-07-16 11:48:50 -0700

    Added base files

---
+++ NaturallyMee/NaturallyMee/viewController/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NaturallyMee
+//
+//  Created by Sarin Swift on 7/16/18.
+//  Copyright © 2018 Sarin Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ NaturallyMee/NaturallyMee/viewController/productDisplayViewController.swift
@@ -0,0 +1,9 @@
+//
+//  productDisplayViewController.swift
+//  NaturallyMee
+//
+//  Created by Sarin Swift on 7/16/18.
+//  Copyright © 2018 Sarin Swift. All rights reserved.
+//
+
+import Foundation
